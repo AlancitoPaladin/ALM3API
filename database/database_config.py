1a34412e2ebc88d0c9eed71f360f2f0eb2d8c3bd
AlancitoPaladin <alancarlos032104@gmail.com>
2025-12-01 15:59:31 -0600

    Adding Semi-final changes

--- database/database_config.py
+++ database/database_config.py
@@ -8,7 +8,7 @@ mongo = PyMongo()
 
 
 def init_db(app):
-    mongo_uri = os.getenv("MONGO_URI2")
+    mongo_uri = os.getenv("MONGO_URI")
     if not mongo_uri:
         raise ValueError("La variable de entorno MONGO_URI no está definida.")
     app.config["MONGO_URI"] = mongo_uri
