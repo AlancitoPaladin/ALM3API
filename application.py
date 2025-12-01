63a838dd9768d188c0d65557b8a8e2add26779f2
AlancitoPaladin <alancarlos032104@gmail.com>
2025-12-01 17:02:59 -0600

    Adding Semi-final changes

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
 from api.factory import create_app
 from database.database_config import init_db
 
-app = create_app()
+application = create_app()
 
 app.config["DEBUG"] = True
 
-init_db(app)
+init_db(application)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
